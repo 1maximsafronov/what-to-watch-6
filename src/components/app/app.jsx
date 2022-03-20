2cc02baceaa19fa8b987aaf7069fbbef0ba3c9a9
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-21 00:36:35 +0700

    Подключает prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import MovieCard from "../movie-card/movie-card.jsx";
 import PageContent from "../page-content/page-content.jsx";
 
+const moviesList = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`
+];
+
 const App = () => {
   return (
     <React.Fragment>
       <MovieCard />
-      <PageContent />
+      <PageContent
+        movies={moviesList}
+      />
     </React.Fragment>
   );
 };
