2997928ed2a26ce5a3255e1f7fcf4dcb1674236b
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-23 00:22:36 +0700

    Добавляет маршруты приложения

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,23 +5,39 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 import MovieDetailsPage from "../movie-details-page/movie-details-page.jsx";
 
+const AppRoutes = {
+  MAIN: `/`,
+  LOGIN: `/login`,
+  MY_LIST: `/mylist`,
+  FILM: `/films`,
+  REVIEW: `/film/review`,
+  PLAYER: `/player`
+};
+
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-
-  }
-
   render() {
     const {movies} = this.props;
 
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={
+          <Route exact path={AppRoutes.MAIN} element={
             <MainPage movies={movies}/>
           }/>
-          <Route exact path="/movie" element={
+          <Route exact path={AppRoutes.LOGIN} element={
+            <h1>Страница авторизации</h1>
+          }/>
+          <Route exact path={AppRoutes.MY_LIST} element={
+            <h1>Мой список фильмов</h1>
+          }/>
+          <Route exact path={AppRoutes.FILM} element={
             <MovieDetailsPage />
+          }/>
+          <Route exact path={AppRoutes.REVIEW} element={
+            <h1>Страница с отзывом</h1>
+          }/>
+          <Route exact path={AppRoutes.PLAYER} element={
+            <h1>Страница плеера</h1>
           }/>
         </Routes>
       </BrowserRouter>
