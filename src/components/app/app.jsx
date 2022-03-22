096c69158719ec56eb2fc208c722b0672a1d9365
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-22 22:23:23 +0700

    ...

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import MainPage from "../main-page/main-page.jsx";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 import MovieDetailsPage from "../movie-details-page/movie-details-page.jsx";
 
-const App = (props) => {
-  const {movies} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={
-          <MainPage movies={movies}/>
-        }/>
-        <Route exact path="/miveu" element={
-          <MovieDetailsPage />
-        }/>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+  }
+
+  render() {
+    const {movies} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={
+            <MainPage movies={movies}/>
+          }/>
+          <Route exact path="/miveu" element={
+            <MovieDetailsPage />
+          }/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
 
 App.propTypes = {
   movies: PropTypes.array.isRequired,
