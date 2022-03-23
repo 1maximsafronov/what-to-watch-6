b237e12a436b59b999ddd3fe3e3af5c4de967be4
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-23 10:46:24 +0700

    Добавляет меню карточки фильма

--- src/components/movie-details-page/movie-details-page.jsx
+++ src/components/movie-details-page/movie-details-page.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, PureComponent} from "react";
 
 import Overview from "../overview/overview.jsx";
 // import Details from "../details/details.jsx";
@@ -7,92 +7,91 @@ import Logo from "../logo/logo.jsx";
 import PageFooter from "../page-footer/page-footer.jsx";
 import MoviesList from "../movies-list/movies-list.jsx";
 import UserBlock from "../user-block/user-block.jsx";
+import MovieNav from "../movie-nav/movie-nav.jsx";
 
 import movies from "../../mocks/films.js";
 
-const MovieDetailsPage = () => {
-  return (
-    <Fragment>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__hero">
-          <div className="movie-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-          </div>
+class MovieDetailsPage extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab: ``
+    };
+  }
 
-          <h1 className="visually-hidden">WTW</h1>
+  render() {
+    return (
+      <Fragment>
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__hero">
+            <div className="movie-card__bg">
+              <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+            </div>
 
-          <header className="page-header movie-card__head">
-            <Logo href="main.html"/>
+            <h1 className="visually-hidden">WTW</h1>
 
-            <UserBlock />
-          </header>
+            <header className="page-header movie-card__head">
+              <Logo href="main.html"/>
 
-          <div className="movie-card__wrap">
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">Drama</span>
-                <span className="movie-card__year">2014</span>
-              </p>
+              <UserBlock />
+            </header>
 
-              <div className="movie-card__buttons">
-                <button className="btn btn--play movie-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list movie-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+            <div className="movie-card__wrap">
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">Drama</span>
+                  <span className="movie-card__year">2014</span>
+                </p>
+
+                <div className="movie-card__buttons">
+                  <button className="btn btn--play movie-card__button" type="button">
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button className="btn btn--list movie-card__button" type="button">
+                    <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add"></use>
+                    </svg>
+                    <span>My list</span>
+                  </button>
+                  <a href="add-review.html" className="btn movie-card__button">Add review</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218"
-                height="327" />
-            </div>
+          <div className="movie-card__wrap movie-card__translate-top">
+            <div className="movie-card__info">
+              <div className="movie-card__poster movie-card__poster--big">
+                <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218"
+                  height="327" />
+              </div>
 
-            <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
+              <div className="movie-card__desc">
+                <MovieNav />
 
-              <Overview />
+                <Overview />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">More like this</h2>
-          <MoviesList movies={movies}/>
         </section>
 
-        <PageFooter logoLink="main.html" />
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">More like this</h2>
+            <MoviesList movies={movies}/>
+          </section>
 
-      </div>
-    </Fragment>
-  );
-};
+          <PageFooter logoLink="main.html" />
+
+        </div>
+      </Fragment>
+    );
+  }
+}
+
 
 export default MovieDetailsPage;
