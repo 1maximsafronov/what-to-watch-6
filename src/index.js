c01fb37e4644b698f52a23d9d1787037192eb9e6
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-23 15:46:09 +0700

    Добавляет поддержку redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDom.render(
     <Provider store={store}>
