6a4bfcbe8d6154a4656d55e6cf83bb0824a20bbe
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-24 20:24:45 +0700

    Добавляет маршруты страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import MainPage from "../main-page/main-page";
+import LoginPage from "../login-page/login-page";
+import MyListPage from "../my-list-page/my-list-page";
+import MoviePage from "../movie-page/movie-page";
+import AddReviewPage from "../add-review-page/add-review-page";
+import MoviePlayerPage from "../movie-player-page/movie-player-page";
 
 const App = () => {
   return (
-    <h1>Тут App</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListPage />
+        </Route>
+        <Route exact path="/films">
+          <MoviePage />
+        </Route>
+        <Route exact path="/films/review">
+          <AddReviewPage />
+        </Route>
+        <Route exact path="/player">
+          <MoviePlayerPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
