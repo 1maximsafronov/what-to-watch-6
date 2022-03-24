6cf21841b02e151e7d39a74934aaf345bb56ff41
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-25 00:14:45 +0700

    Добавляет компонент GenresListItem и state для GenresList

---
+++ src/components/genres-list-item/genres-list-item.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+const GenreListItem = (props) => {
+  const {genre, isActive, onClick} = props;
+  const className = classNames(
+      `catalog__genres-item`,
+      {"catalog__genres-item--active": isActive}
+  );
+
+  const clickHandler = (evt) => {
+    evt.preventDefault();
+    onClick();
+  };
+
+  return (
+    <li className={className}>
+      <a className="catalog__genres-link" href="#"
+        onClick={clickHandler}
+      >{genre}</a>
+    </li>
+  );
+};
+
+GenreListItem.propTypes = {
+  genre: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default GenreListItem;
--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -1,39 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import GenreListItem from "../genres-list-item/genres-list-item";
+
+const genresList = [
+  `All genres`,
+  `Comedies`,
+  `Crime`,
+  `Documentary`,
+  `Dramas`,
+  `Horror`,
+  `Kids & Family`,
+  `Romance`,
+  `Sci-Fi`,
+  `Thrillers`
+];
 
 const GenresList = () => {
+  const [currentGenre, setGenre] = useState(`All genres`);
 
   return (
     <ul className="catalog__genres-list">
-      <li className="catalog__genres-item catalog__genres-item--active">
-        <a href="#" className="catalog__genres-link">All genres</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Comedies</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Crime</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Documentary</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Dramas</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Horror</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Kids & Family</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Romance</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Sci-Fi</a>
-      </li>
-      <li className="catalog__genres-item">
-        <a href="#" className="catalog__genres-link">Thrillers</a>
-      </li>
+      {genresList.map((genre, index) => (
+        <GenreListItem key={`genre-${index}`}
+          genre={genre}
+          isActive={genre === currentGenre}
+          onClick={() => setGenre(genre)}
+        />
+      ))}
     </ul>
   );
 };
