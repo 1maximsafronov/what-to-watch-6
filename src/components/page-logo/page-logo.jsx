c7c0d5bc469116c0e862affc59eb35504d111928
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-25 02:16:40 +0700

    Делает page-logo ссылкой на главную

--- src/components/page-logo/page-logo.jsx
+++ src/components/page-logo/page-logo.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
 const PageLogo = (props) => {
-  const {link, light} = props;
+  const {light} = props;
 
   const linkClass = classNames(`logo__link`, {"logo__link--light": light});
 
   return (
     <div className="logo">
-      <a href={link} className={linkClass}>
+      <Link to="/" className={linkClass}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
-      </a>
+      </Link>
     </div>
   );
 };
