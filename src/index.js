eaa30a43dbde7286915737aeb8b742b9307b6563
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-24 11:27:34 +0700

    Добавляет api

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./components/app/app.jsx";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./reducer.js";
-
+import {createAPI} from "./api.js";
 
 const store = createStore(
     reducer,
