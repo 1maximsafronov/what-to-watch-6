59c79eb50b706ef6f315d50a3b9ea2ece1476465
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-24 12:32:54 +0700

    ...

--- src/reducer/data/data.js
+++ src/reducer/data/data.js
@@ -10,6 +10,7 @@ const initialState = {
 const ActionType = {
   SET_GENRE_FILTER: `SET_GENRE_FILTER`,
   LOAD_MOVIES: `LOAD_MOVIES`,
+  LOAD_ONE_MOVIE: `LOAD_ONE_MOVIE`,
 };
 
 const ActionCreater = {
@@ -19,10 +20,16 @@ const ActionCreater = {
       payload: filterType
     };
   },
-  loadMovies(data) {
+  loadMovies(movies) {
     return {
       type: ActionType.LOAD_MOVIES,
-      payload: data
+      payload: movies
+    };
+  },
+  loadOneMovie(movie) {
+    return {
+      type: ActionType.LOAD_ONE_MOVIE,
+      payload: movie
     };
   }
 };
@@ -34,11 +41,54 @@ const Operation = {
         const moveis = response.data.map(adaptMovieToClient);
         dispatch(ActionCreater.loadMovies(moveis));
       });
+  },
+  loadOneMovie: (id) => (dispatch, getState, api) => {
+    return api.get(`/films/${id}`)
+      .then((response) => {
+        const movie = adaptMovieToClient(response.data);
+        dispatch(ActionCreater.loadOneMovie(movie));
+      });
+  },
+  loadSimilarMovies: (id) => (dispatch, getState, api) => {
+    return api.get(`/films/${id}/similar`)
+      .then((response) => {
+        // TODO: Добавить обработку загрузки похожих фильмов
+      });
+  },
+  loadFavoriteMovies: () => (dispatch, getState, api) =>{
+    return api.get(`/favorite`)
+      .then((response) => {
+        // TODO: Добавить обработку загрузки списка пользователя
+      });
+  },
+  addToFavorite: (id, status) => (dispatch, getState, api) => {
+    status = status ? 1 : 0;
+
+    return api.post(`/favorite/${id}/${status}`)
+      .then((response) => {
+        // TODO: Добавить обработку добавления фильма в фавориты
+      });
+  },
+  loadComments: (filmId)=> (dispatch, getState, api) => {
+    return api.get(`/comments/${filmId}`)
+      .then((response) => {
+        // TODO: Добавить обработку загруженых коментов
+      });
+  },
+  sendNewComment: (filmId, newComment) =>(dispatch, getState, api) => {
+    return api.post(`/comments/${filmId}`, {
+      rating: newComment.rating,
+      comment: newComment.comment
+    })
+    .then((response) => {
+      //
+    });
   }
 };
 
 
 const reducer = (state = initialState, action) => {
+  // TODO: Добавить обработку Action : LOAD_ONE_MOVIE
   switch (action.type) {
     case ActionType.SET_GENRE_FILTER:
       return extendObject(state, {currentGenreFilter: action.payload});
