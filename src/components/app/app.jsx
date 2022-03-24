5702ee4d5349efb7ca7202f32b28141ff4fc93c9
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-24 19:42:04 +0700

    Меняет структуру проекта

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,62 +1,10 @@
-import {Routes, Route, BrowserRouter} from "react-router-dom";
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {getMovies} from "../../reducer/data/selector.js";
 
-import MainPage from "../main-page/main-page.jsx";
-import LoginPage from "../login-page/login-page.jsx";
-import MovieDetailsPage from "../movie-details-page/movie-details-page.jsx";
-import ReviewPage from "../review-page/review-page.jsx";
-import Player from "../player/player.jsx";
-import MyList from "../my-list/my-list.jsx";
-
-const AppRoutes = {
-  MAIN: `/`,
-  LOGIN: `/login`,
-  MY_LIST: `/mylist`,
-  FILM: `/films`,
-  REVIEW: `/review`,
-  PLAYER: `/player`
+const App = () => {
+  return (
+    <h1>Тут App</h1>
+  );
 };
 
-class App extends PureComponent {
-  render() {
-    const {movies} = this.props;
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path={AppRoutes.MAIN} element={
-            <MainPage movies={movies}/>
-          }/>
-          <Route exact path={AppRoutes.LOGIN} element={
-            <LoginPage />
-          }/>
-          <Route exact path={AppRoutes.MY_LIST} element={
-            <MyList />
-          }/>
-          <Route exact path={AppRoutes.FILM} element={
-            <MovieDetailsPage />
-          }/>
-          <Route exact path={AppRoutes.REVIEW} element={
-            <ReviewPage />
-          }/>
-          <Route exact path={AppRoutes.PLAYER} element={
-            <Player />
-          }/>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
-}
-
-
-App.propTypes = {
-  movies: PropTypes.array.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  movies: getMovies(state)
-});
-
-export default connect(mapStateToProps, null)(App);
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
-  entry: `./src/index.js`,
+  entry: './src/index.js',
   output: {
-    filename: `bundle.js`,
-    path: path.join(__dirname, `./public`)
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, `./public`),
+    contentBase: path.resolve(__dirname, 'public'),
     open: true,
-    // inline: false,
-    hot: true,
-    port: 8080,
+    port: 1337,
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -20,10 +18,13 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: `babel-loader`,
+          loader: 'babel-loader',
         },
       }
     ],
   },
-  devtool: `source-map`,
-}
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+};
