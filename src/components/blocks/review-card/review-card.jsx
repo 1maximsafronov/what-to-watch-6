dfce97efbd2152ae291ac548e5ec79d62ea54484
Maxim Safronov <1maximsafronov@gmail.com>
2022-03-27 18:30:56 +0700

    Подключает moment и выводит форматированную дату

--- src/components/blocks/review-card/review-card.jsx
+++ src/components/blocks/review-card/review-card.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import moment from "moment";
 
 const ReviewCard = ({comment}) => {
-  const {author, text, rating} = comment;
-  // TODO: ДОбавить вывод даты коментария
+  const {author, text, rating, date} = comment;
+
+  const formatDate = moment(date).format(`MMMM DD, YYYY`);
+  const dateTime = moment(date).format(`YYYY-MM-DD`);
+
   return (
     <div className="review">
       <blockquote className="review__quote">
         <p className="review__text">{text}</p>
         <footer className="review__details">
           <cite className="review__author">{author}</cite>
-          <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+          <time className="review__date" dateTime={dateTime}>{formatDate}</time>
         </footer>
       </blockquote>
 
